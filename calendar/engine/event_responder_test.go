57b7c7ae83fae4194be9adc5f9bcb86de61b0a50
kshitij katiyar <kshitij.katiyar@brightscout.com>
2024-10-14 15:00:28 +0530

    [MM-838]: updated the testcases struct

--- calendar/engine/event_responder_test.go
+++ calendar/engine/event_responder_test.go
@@ -22,33 +22,27 @@ func TestAcceptEvent(t *testing.T) {
 
 	tests := []struct {
 		name                 string
-		eventID              string
 		setupMock            func()
-		expectedErrorMessage string
 		assertion            func(err error)
 	}{
 		{
 			name:    "error filtering with user",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = nil
 				mockStore.EXPECT().LoadUser("testMMUserID").Return(nil, errors.New("error filtering user")).Times(1)
 			},
-			expectedErrorMessage: "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user",
 			assertion: func(err error) {
 				require.Error(t, err)
-				require.EqualError(t, err, "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user")
+				require.ErrorContains(t, err, "error filtering user")
 			},
 		},
 		{
 			name:    "error accepting event",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockPluginAPI.EXPECT().GetMattermostUser("testMMUserID")
-				mockClient.EXPECT().AcceptEvent("testRemoteID", "testEventID").Return(errors.New("unable to accept event")).Times(1)
+				mockClient.EXPECT().AcceptEvent("testRemoteID", "mockEventID").Return(errors.New("unable to accept event")).Times(1)
 			},
-			expectedErrorMessage: "unable to accept event",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "unable to accept event")
@@ -56,24 +50,21 @@ func TestAcceptEvent(t *testing.T) {
 		},
 		{
 			name:    "successful event acceptance",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockPluginAPI.EXPECT().GetMattermostUser("testMMUserID")
-				mockClient.EXPECT().AcceptEvent("testRemoteID", "testEventID").Return(nil).Times(1)
+				mockClient.EXPECT().AcceptEvent("testRemoteID", "mockEventID").Return(nil).Times(1)
 			},
-			expectedErrorMessage: "",
 			assertion: func(err error) {
 				require.NoError(t, err)
 			},
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.setupMock()
 
-			err := mscalendar.AcceptEvent(user, tt.eventID)
+			err := mscalendar.AcceptEvent(user, "mockEventID")
 
 			tt.assertion(err)
 		})
@@ -90,33 +81,27 @@ func TestDeclineEvent(t *testing.T) {
 
 	tests := []struct {
 		name                 string
-		eventID              string
 		setupMock            func()
-		expectedErrorMessage string
 		assertion            func(err error)
 	}{
 		{
 			name:    "error filtering with user",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = nil
 				mockStore.EXPECT().LoadUser("testMMUserID").Return(nil, errors.New("error filtering user")).Times(1)
 			},
-			expectedErrorMessage: "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user",
 			assertion: func(err error) {
 				require.Error(t, err)
-				require.EqualError(t, err, "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user")
+				require.ErrorContains(t, err, "error filtering user")
 			},
 		},
 		{
 			name:    "error declining event",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockPluginAPI.EXPECT().GetMattermostUser("testMMUserID")
 				mockClient.EXPECT().DeclineEvent("testRemoteID", "testEventID").Return(errors.New("unable to decline event")).Times(1)
 			},
-			expectedErrorMessage: "unable to decline event",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "unable to decline event")
@@ -124,24 +109,21 @@ func TestDeclineEvent(t *testing.T) {
 		},
 		{
 			name:    "successful event decline",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockPluginAPI.EXPECT().GetMattermostUser("testMMUserID")
 				mockClient.EXPECT().DeclineEvent("testRemoteID", "testEventID").Return(nil).Times(1)
 			},
-			expectedErrorMessage: "",
 			assertion: func(err error) {
 				require.NoError(t, err)
 			},
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.setupMock()
 
-			err := mscalendar.DeclineEvent(user, tt.eventID)
+			err := mscalendar.DeclineEvent(user, "testEventID")
 
 			tt.assertion(err)
 		})
@@ -158,33 +140,27 @@ func TestTentativelyAcceptEvent(t *testing.T) {
 
 	tests := []struct {
 		name                 string
-		eventID              string
 		setupMock            func()
-		expectedErrorMessage string
 		assertion            func(err error)
 	}{
 		{
 			name:    "error filtering with user",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = nil
 				mockStore.EXPECT().LoadUser("testMMUserID").Return(nil, errors.New("error filtering user")).Times(1)
 			},
-			expectedErrorMessage: "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user",
 			assertion: func(err error) {
 				require.Error(t, err)
-				require.EqualError(t, err, "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user")
+				require.ErrorContains(t, err, "error filtering user")
 			},
 		},
 		{
 			name:    "error tentatively accepting event",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockPluginAPI.EXPECT().GetMattermostUser("testMMUserID")
 				mockClient.EXPECT().TentativelyAcceptEvent("testRemoteID", "testEventID").Return(errors.New("unable to tentatively accept event")).Times(1)
 			},
-			expectedErrorMessage: "unable to tentatively accept event",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "unable to tentatively accept event")
@@ -192,24 +168,21 @@ func TestTentativelyAcceptEvent(t *testing.T) {
 		},
 		{
 			name:    "successful tentative event acceptance",
-			eventID: "testEventID",
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockPluginAPI.EXPECT().GetMattermostUser("testMMUserID")
 				mockClient.EXPECT().TentativelyAcceptEvent("testRemoteID", "testEventID").Return(nil).Times(1)
 			},
-			expectedErrorMessage: "",
 			assertion: func(err error) {
 				require.NoError(t, err)
 			},
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.setupMock()
 
-			err := mscalendar.TentativelyAcceptEvent(user, tt.eventID)
+			err := mscalendar.TentativelyAcceptEvent(user, "testEventID")
 
 			tt.assertion(err)
 		})
@@ -226,138 +199,117 @@ func TestRespondToEvent(t *testing.T) {
 
 	tests := []struct {
 		name                 string
-		eventID              string
 		response             string
 		setupMock            func()
-		expectedErrorMessage string
 		assertion            func(err error)
 	}{
 		{
-			name:                 "error - not responded is an invalid response",
-			eventID:              "testEventID",
+			name:                 "invalid response error",
 			response:             OptionNotResponded,
 			setupMock:            func() {},
-			expectedErrorMessage: "not responded is not a valid response",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "not responded is not a valid response")
 			},
 		},
 		{
-			name:     "error - invalid response string",
-			eventID:  "testEventID",
+			name:     "invalid response string",
 			response: "InvalidResponse",
 			setupMock: func() {
 				mockPluginAPI.EXPECT().GetMattermostUser("testMMUserID").Return(&model.User{Id: "testMMUserID"}, nil)
 			},
-			expectedErrorMessage: "InvalidResponse is not a valid response",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "InvalidResponse is not a valid response")
 			},
 		},
 		{
-			name:     "error - filtering fails",
-			eventID:  "testEventID",
+			name:     "error filtering user",
 			response: OptionYes,
 			setupMock: func() {
 				user.User = nil
 				mockStore.EXPECT().LoadUser("testMMUserID").Return(nil, errors.New("error filtering user")).Times(1)
 			},
-			expectedErrorMessage: "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user",
 			assertion: func(err error) {
 				require.Error(t, err)
-				require.EqualError(t, err, "It looks like your Mattermost account is not connected to testDisplayName. Please connect your account using `/testCommandTrigger connect`.: error filtering user")
+				require.ErrorContains(t, err, "error filtering user")
 			},
 		},
 		{
-			name:     "success - accepted event",
-			eventID:  "testEventID",
+			name:     "success accepting event",
 			response: OptionYes,
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockClient.EXPECT().AcceptEvent("testRemoteID", "testEventID").Return(nil).Times(1)
 			},
-			expectedErrorMessage: "",
 			assertion: func(err error) {
 				require.NoError(t, err)
 			},
 		},
 		{
-			name:     "error - accept event fails",
-			eventID:  "testEventID",
+			name:     "error accepting event",
 			response: OptionYes,
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockClient.EXPECT().AcceptEvent("testRemoteID", "testEventID").Return(errors.New("unable to accept event")).Times(1)
 			},
-			expectedErrorMessage: "unable to accept event",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "unable to accept event")
 			},
 		},
 		{
-			name:     "success - declined event",
-			eventID:  "testEventID",
+			name:     "success declining event",
 			response: OptionNo,
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockClient.EXPECT().DeclineEvent("testRemoteID", "testEventID").Return(nil).Times(1)
 			},
-			expectedErrorMessage: "",
 			assertion: func(err error) {
 				require.NoError(t, err)
 			},
 		},
 		{
-			name:     "error - decline event fails",
-			eventID:  "testEventID",
+			name:     "error declining event",
 			response: OptionNo,
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockClient.EXPECT().DeclineEvent("testRemoteID", "testEventID").Return(errors.New("unable to decline event")).Times(1)
 			},
-			expectedErrorMessage: "unable to decline event",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "unable to decline event")
 			},
 		},
 		{
-			name:     "success - tentatively accepted event",
-			eventID:  "testEventID",
+			name:     "success tentatively accepting event",
 			response: OptionMaybe,
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockClient.EXPECT().TentativelyAcceptEvent("testRemoteID", "testEventID").Return(nil).Times(1)
 			},
-			expectedErrorMessage: "",
 			assertion: func(err error) {
 				require.NoError(t, err)
 			},
 		},
 		{
-			name:     "error - tentatively accept event fails",
-			eventID:  "testEventID",
+			name:     "error tentatively accepting event",
 			response: OptionMaybe,
 			setupMock: func() {
 				user.User = &store.User{Settings: store.Settings{}, Remote: &remote.User{ID: "testRemoteID"}}
 				mockClient.EXPECT().TentativelyAcceptEvent("testRemoteID", "testEventID").Return(errors.New("unable to tentatively accept event")).Times(1)
 			},
-			expectedErrorMessage: "unable to tentatively accept event",
 			assertion: func(err error) {
 				require.Error(t, err)
 				require.EqualError(t, err, "unable to tentatively accept event")
 			},
 		},
 	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.setupMock()
 
-			err := mscalendar.RespondToEvent(user, tt.eventID, tt.response)
+			err := mscalendar.RespondToEvent(user, "testEventID", tt.response)
 
 			tt.assertion(err)
 		})
