4ec6bf6df293535a625ade2551608d41504aff03
kshitij katiyar <kshitij.katiyar@brightscout.com>
2024-11-12 19:52:26 +0530

    [MM-853]: review fixes

--- calendar/store/test_util.go
+++ calendar/store/test_util.go
@@ -5,9 +5,19 @@ import (
 
 	"github.com/golang/mock/gomock"
 
+	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/remote"
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/testutil"
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/tracker/mock_tracker"
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/utils/bot/mock_bot"
+)
+
+const (
+	MockEventSubscriptionID = "mockEventSubscriptionID"
+	MockSubscriptionID      = "mockSubscriptionID"
+	MockRemoteUserID        = "mockRemoteUserID"
+	MockRemoteID            = "mockRemoteID"
+	MockCreatorID           = "mockCreatorID"
+	MockMMUserID            = "mockMMUserID"
 )
 
 func GetMockSetup(t *testing.T) (*testutil.MockPluginAPI, Store, *mock_bot.MockLogger, *mock_bot.MockLogger, *mock_tracker.MockTracker) {
@@ -20,4 +30,25 @@ func GetMockSetup(t *testing.T) (*testutil.MockPluginAPI, Store, *mock_bot.MockL
 	store := NewPluginStore(mockAPI, mockLogger, mockTracker, false, nil)
 
 	return mockAPI, store, mockLogger, mockLoggerWith, mockTracker
+}
+
+func GetMockUser() *User {
+	return &User{
+		MattermostUserID: MockMMUserID,
+		Settings: Settings{
+			EventSubscriptionID: MockEventSubscriptionID,
+		},
+		Remote: &remote.User{
+			ID: MockRemoteUserID,
+		},
+	}
+}
+
+func GetMockSubscription() *Subscription {
+	return &Subscription{
+		Remote: &remote.Subscription{
+			ID:        MockSubscriptionID,
+			CreatorID: MockCreatorID,
+		},
+	}
 }
