9a6366c06a418e0bf294b9ca46e561beff2dafa1
kshitij katiyar <kshitij.katiyar@brightscout.com>
2024-11-13 12:07:32 +0530

    [MM-854]: review fixes

--- calendar/store/oauth2_store_test.go
+++ calendar/store/oauth2_store_test.go
@@ -6,22 +6,22 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
+	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/testutil"
+
 	"github.com/mattermost/mattermost/server/public/model"
 )
 
 func TestVerifyOAuth2State(t *testing.T) {
-	mockAPI, store, _, _, _ := GetMockSetup(t)
-
 	tests := []struct {
 		name       string
-		setup      func()
+		setup      func(*testutil.MockPluginAPI)
 		assertions func(*testing.T, error)
 	}{
 		{
 			name: "Error loading state",
-			setup: func() {
-				mockAPI.On("KVGet", mock.Anything).Return(nil, &model.AppError{Message: "Error getting state"}).Times(1)
-				mockAPI.On("KVDelete", mock.Anything).Return(nil)
+			setup: func(mockAPI *testutil.MockPluginAPI) {
+				mockAPI.On("KVGet", "oauth2_fb89cea34670836627b56ad5b94ce5e3").Return(nil, &model.AppError{Message: "Error getting state"}).Times(1)
+				mockAPI.On("KVDelete", "oauth2_fb89cea34670836627b56ad5b94ce5e3").Return(nil)
 			},
 			assertions: func(t *testing.T, err error) {
 				require.Error(t, err)
@@ -30,8 +30,8 @@ func TestVerifyOAuth2State(t *testing.T) {
 		},
 		{
 			name: "Invalid Oauth state",
-			setup: func() {
-				mockAPI.On("KVGet", mock.Anything).Return([]byte("invalidState"), nil).Times(1)
+			setup: func(mockAPI *testutil.MockPluginAPI) {
+				mockAPI.On("KVGet", "oauth2_fb89cea34670836627b56ad5b94ce5e3").Return([]byte("invalidState"), nil).Times(1)
 			},
 			assertions: func(t *testing.T, err error) {
 				require.Error(t, err)
@@ -40,9 +40,8 @@ func TestVerifyOAuth2State(t *testing.T) {
 		},
 		{
 			name: "Successfull Oauth state verification",
-			setup: func() {
-				mockAPI.ExpectedCalls = nil
-				mockAPI.On("KVGet", mock.Anything).Return([]byte("mockState"), nil).Times(1)
+			setup: func(mockAPI *testutil.MockPluginAPI) {
+				mockAPI.On("KVGet", "oauth2_fb89cea34670836627b56ad5b94ce5e3").Return([]byte(MockState), nil).Times(1)
 			},
 			assertions: func(t *testing.T, err error) {
 				require.Nil(t, err)
@@ -51,9 +50,10 @@ func TestVerifyOAuth2State(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.setup()
+			mockAPI, store, _, _, _ := GetMockSetup(t)
+			tt.setup(mockAPI)
 
-			err := store.VerifyOAuth2State("mockState")
+			err := store.VerifyOAuth2State(MockState)
 
 			tt.assertions(t, err)
 
@@ -63,17 +63,15 @@ func TestVerifyOAuth2State(t *testing.T) {
 }
 
 func TestStoreOAuth2State(t *testing.T) {
-	mockAPI, store, _, _, _ := GetMockSetup(t)
-
 	tests := []struct {
 		name       string
-		setup      func()
+		setup      func(*testutil.MockPluginAPI)
 		assertions func(*testing.T, error)
 	}{
 		{
 			name: "Error loading state",
-			setup: func() {
-				mockAPI.On("KVSetWithExpiry", mock.Anything, mock.Anything, mock.Anything).Return(&model.AppError{Message: "Error loading state"}).Times(1)
+			setup: func(mockAPI *testutil.MockPluginAPI) {
+				mockAPI.On("KVSetWithExpiry", "oauth2_fb89cea34670836627b56ad5b94ce5e3", mock.Anything, int64(oAuth2StateTimeToLive)).Return(&model.AppError{Message: "Error loading state"}).Times(1)
 			},
 			assertions: func(t *testing.T, err error) {
 				require.Error(t, err)
@@ -82,9 +80,8 @@ func TestStoreOAuth2State(t *testing.T) {
 		},
 		{
 			name: "Successfull Oauth state verification",
-			setup: func() {
-				mockAPI.ExpectedCalls = nil
-				mockAPI.On("KVSetWithExpiry", mock.Anything, mock.Anything, mock.Anything).Return(nil).Times(1)
+			setup: func(mockAPI *testutil.MockPluginAPI) {
+				mockAPI.On("KVSetWithExpiry", "oauth2_fb89cea34670836627b56ad5b94ce5e3", mock.Anything, int64(oAuth2StateTimeToLive)).Return(nil).Times(1)
 			},
 			assertions: func(t *testing.T, err error) {
 				require.Nil(t, err)
@@ -93,9 +90,10 @@ func TestStoreOAuth2State(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.setup()
+			mockAPI, store, _, _, _ := GetMockSetup(t)
+			tt.setup(mockAPI)
 
-			err := store.StoreOAuth2State("mockState")
+			err := store.StoreOAuth2State(MockState)
 
 			tt.assertions(t, err)
 
